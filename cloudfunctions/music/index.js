d8842ec55c9486f0354c9a4e66934cec19c891c7
likanghua <lkh19890913@163.com>
2020-12-31 23:37:16 +0800

    feat: add music-item

--- cloudfunctions/music/index.js
+++ cloudfunctions/music/index.js
@@ -2,6 +2,11 @@
 const cloud = require('wx-server-sdk')
 
 const TcbRouter = require('tcb-router')
+
+const rp = require('request-promise')
+
+const BASE_URL = 'http://47.115.57.59:3000'
+
 cloud.init()
 
 // 云函数入口函数
@@ -21,6 +26,14 @@ exports.main = async (event, context) => {
     const { total } = await cloud.database().collection('playlist').count()
 
     ctx.body = { data, total }
+  })
+
+  app.router('musiclist', async (ctx, next) => {
+    
+    const res = await rp(`${BASE_URL}/playlist/detail?id=${parseInt(event.playlistId)}`).then(res => JSON.parse(res))
+    
+    ctx.body = res
+  
   })
 
   return app.serve()
