aad224a42a7f74584b8d68c5afdcc6c4a0ec7383
likanghua <lkh19890913@163.com>
2021-01-01 23:17:37 +0800

    feat: add player

--- miniprogram/components/music-item/index.js
+++ miniprogram/components/music-item/index.js
@@ -4,20 +4,30 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    musiclist: {
+      type:Array
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    musicid: -1
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    handleSelected(e) {
+      const musicid = e.currentTarget.dataset.musicid
+      this.setData({
+        musicid
+      })
+      wx.navigateTo({
+        url: `/pages/player/player?musicId=${musicid}`,
+      })
+    }
   }
 })
